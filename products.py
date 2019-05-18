45a75faa2b8a00541133bc1e0d5eb1b8b4880b04
Adel Lassag <adetech.aadl@gmail.com>
2019-05-19 00:15:14 +0100

    feat: Create product class

---
+++ products.py
@@ -0,0 +1,9 @@
+class Products:
+    def __init__(self, id, name, cat, descr, photo_url, price):
+        self.id = id
+        self.name = name
+        self.cat = cat
+        self.descr = descr
+        self.photo_url = photo_url
+        self.price = price
+

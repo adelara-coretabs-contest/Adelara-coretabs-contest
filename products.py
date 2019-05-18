463ec1c707279c919d8c32258e950ad4f758d94e
Adel Lassag <adetech.aadl@gmail.com>
2019-05-19 00:18:21 +0100

    feat: Create product store class

--- products.py
+++ products.py
@@ -7,3 +7,16 @@ class Products:
         self.photo_url = photo_url
         self.price = price
 
+
+class ProductStore:
+    def __init__(self, products):
+        self.products = products
+
+    def get_all(self):
+        for product in self.products:
+            print(str(product.id) + '-' + product.name + ': ' + product.descr)
+        return self.products
+
+    def add(self, product):
+
+        return self.products.append(product)

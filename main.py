facce6b92d86fbce6af28319c3bc7c4a03885c1f
Adel Lassag <adetech.aadl@gmail.com>
2019-05-22 03:35:33 +0100

    feat: update order model, detail function & details template

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from meals import Meals, MealsStore
 from orders import Orders, OrdersStore
 
@@ -50,10 +50,9 @@ dummy_meals = [
 meals = MealsStore(dummy_meals)
 meals.get_all()
 
-
 dummy_orders = []
 orders = OrdersStore(dummy_orders)
-orders.add(Orders(1, dummy_meals[0], 3, 'Adel', 'today'))
+orders.add(Orders(1, dummy_meals[0], 3, 'Adel', 'my address', 'today'))
 orders.get_all()
 
 
@@ -63,9 +62,23 @@ def index():
     return render_template('index.html', meals=dummy_meals)
 
 
+app.current_id = 1
 @app.route('/details/<int:id>', methods=["GET", "POST"])
 def details(id):
+    meal = meals.get_details(id)
     if request.method == "GET":
-        meal = meals.get_details(id)
         return render_template('details.html', meal=meal)
+
+    elif request.method == "POST":
+        new_order = Orders(id=app.current_id,
+                        meal=meal,
+                        quantity=int(request.form["quantity"]),
+                        user=request.form["user"],
+                        address=request.form["address"],
+                        date = 'today')
+        app.current_id += 1
+        orders.add(new_order)
+        orders.get_all()
+        return redirect(url_for("index"))
+
 
--- orders.py
+++ orders.py
@@ -1,9 +1,11 @@
 class Orders:
-    def __init__(self, id, meal, quantity, user, date):
+    def __init__(self, id, meal, quantity, user, address, date):
         self.id = id
-        self.meal = meal.id
+        self.meal_id = meal.id
+        self.meal_name = meal.name
         self.quantity = quantity
         self.user = user
+        self.address = address
         self.date = date
         self.amount = meal.price * quantity
 
@@ -17,8 +19,10 @@ class OrdersStore:
         for order in self.orders:
             print('******************')
             print(str(order.id) + '-' + order.user + '.')
+            print('\tMeal' + str(order.meal_name))
             print('\tQuantity: ' + str(order.quantity))
             print('\tAmount: $' + str(order.amount))
+            print('\tAddress: ' + order.address)
         return self.orders
 
     def get_order(self, id):

016eb6335be9eb9e36f353bee0ea06d207232d6d
Lara Darine <delara.risal2017@gmail.com>
2019-05-25 15:15:25 +0100

    Update static assets :zap:

--- templates/static/js/app.js
+++ templates/static/js/app.js
@@ -13,8 +13,25 @@ hamburger.addEventListener('click', () => {
 });
 
 let downToMeals = document.querySelector('#scroll-down');
-downToMeals.addEventListener('click', ()=> {
+let scrollToMeals = ()=> {
     let meals = document.querySelector('#main');
     downToMeals.scrollIntoView(meals);
     console.log('meals');
-})
+}
+
+let toggleForm = (element)=> {
+    let form = document.querySelector('.form');
+    let ingredients = document.querySelector('#ingredients');
+    let mealName = document.querySelector('#meal-name');
+    let thisElement = document.querySelector(`#${element}`)
+    console.log(form)
+    form.classList.toggle('hidden');
+    ingredients.classList.toggle('hidden');
+    mealName.classList.toggle('hidden');
+    if (thisElement.textContent == 'Order now') {
+        thisElement.textContent = 'Cancel';
+    } else {
+        thisElement.textContent = 'Order now'
+    }
+    console.log(thisElement)
+}
